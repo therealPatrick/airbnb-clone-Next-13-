21f6aa34f372a06d983a9e90383c86ce77226063
Patrick <malamashangala13@gmail.com>
2023-06-04 00:10:23 +0200

    navbar ui design done

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -2,6 +2,9 @@
 import React from 'react'
 import Container from '../Container'
 import Logo from './Logo'
+import Search from './Search'
+import UserMenu from './UserMenu'
+
 
 const Navbar = () => {
   return (
@@ -19,6 +22,8 @@ const Navbar = () => {
                     '
                     >
                         <Logo />
+                        <Search />
+                        <UserMenu />
                 </div>
             </Container>
         </div>
